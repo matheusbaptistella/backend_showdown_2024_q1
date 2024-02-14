b23c5c8ec7b35cc8d38188db31bd7e8cae49ce28
Matheus Baptistella <mbapts@gmail.com>
2024-02-14 00:14:44 -0300

    Added tests folder

--- v1/src/main.rs
+++ v1/src/main.rs
@@ -1,11 +1,11 @@
+use axum::{Router, routing::get};
 
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/hello", get(|| async { "Hello, World!" }));
 
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!(" >> Listening on {}", listener.local_addr().unwrap());
 
-
-
-
-
-
-fn main() {
-    println!("Hello, world!");
+    axum::serve(listener, app).await.unwrap();
 }
